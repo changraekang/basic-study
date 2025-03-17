1e9222bdc5194591921507e4c8a183f9c515e5e5
changraekang <storkcr@gmail.com>
2025-03-17 21:03:17 +0900

    :lipstick: add Link styling

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,26 +1,31 @@
-import styled from 'styled-components';
+import styled from "styled-components";
+
+import { Link } from "react-router-dom";
 
 // 색상 상수
-import {
-  grey1,
-  grey2,
-  white,
-  primary,
-  primaryHover,
-} from '@/styles/colors';
+import { grey1, grey2, white, primary, primaryHover } from "@/styles/colors";
 
-
+/* 공통 모듈 */
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-top: 50px;
-  background-color: ${primary}20; 
+  background-color: ${primary}20;
 `;
 
 export const Title = styled.h1`
   margin-bottom: 20px;
   color: ${grey1};
+`;
+
+export const StyledLink = styled(Link)`
+  color: ${primary};
+  text-decoration: none;
+  font-size: 3.2rem;
+  &:hover {
+    color: ${primaryHover};
+  }
 `;
 
 export const Form = styled.form`
@@ -43,11 +48,11 @@ export const Button = styled.button`
   font-size: 16px;
   border: none;
   color: ${white};
-  background: ${primary};       
+  background: ${primary};
   border-radius: 4px;
 
   &:hover {
-    background: ${primaryHover}; 
+    background: ${primaryHover};
   }
 `;
 export const List = styled.ul`
